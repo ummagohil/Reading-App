f550ca985f10455408915ca2c9a9479f6f51f097
Umma Gohil <756D6D61@gmail.com>
2020-04-06 17:38:52 +0100

    Basic routing

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <h2>Home</h2>
+        <h2>
+          <Link to="/">Home</Link>
+        </h2>
+        <h2>
+          <Link to="/Add">Add</Link>
+        </h2>
+        <h2>
+          <Link to="/Edit">Edit</Link>
+        </h2>
       </div>
     )
   }
