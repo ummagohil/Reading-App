75bcacd67163ae16a0c9e3a5e81b01171a6ce80c
Umma Gohil <756D6D61@gmail.com>
2020-04-06 15:25:04 +0100

    TypeScript switch

---
+++ src/layout/page.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class page extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Add</h2>
+      </div>
+    )
+  }
+}
+
+export default page
