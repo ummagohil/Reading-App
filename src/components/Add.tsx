10a84b209aa8c4d48dbb040069ae97b354a7cd1a
u-gohil <756D6D61@gmail.com>
2020-05-29 13:57:54 +0100

    Save button

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react'
 
 const Add: FC = () => {
-    return <>Add Button</>
+    return( <div className="px-2 "><button className="w-full h-10 rounded bg-corn-500">Add Button</button></div>)
 }
 export default Add
