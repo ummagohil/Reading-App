576ba0137de31589522874569e85913a8fd7de2f
u-gohil <756D6D61@gmail.com>
2020-05-29 14:01:37 +0100

    Info styling

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react'
 
 const Add: FC = () => {
-    return( <div className="px-2 "><button className="w-full h-10 rounded bg-corn-500">Add Button</button></div>)
+    return( <div className="px-2"><button className="w-full h-10 rounded bg-corn-500">Add Button</button></div>)
 }
 export default Add
--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,6 +1,13 @@
 import React, { FC } from 'react'
 
 const Info: FC = () => {
-    return <>Info</>
+    return (
+        <div className="px-2 p-4">
+        <div className=" text-corn-500 rounded bg-black flex">
+        <div className="w-1/2">Book info</div>
+        <div className="w-1/2">Stats info</div>
+    </div>
+    </div>
+    )
 }
 export default Info
