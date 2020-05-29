05543cd2f2d025f47a6bc5dd9c191da25fb21582
u-gohil <756D6D61@gmail.com>
2020-05-29 14:06:51 +0100

    Info border

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -4,8 +4,9 @@ const Info: FC = () => {
     return (
         <div className="px-2 p-4">
         <div className=" text-corn-500 rounded bg-black flex">
-        <div className="w-1/2">Book info</div>
-        <div className="w-1/2">Stats info</div>
+        <div className="w-1/2 p-4">Book info</div>
+        <div className="border-r-2 border-corn-500 p-4"></div>
+        <div className="w-1/2 p-4">Stats info</div>
     </div>
     </div>
     )
