5916f9bba43faf60020063b4bd45c0944ea2f18b
u-gohil <756D6D61@gmail.com>
2020-05-30 10:10:03 +0100

    Temp intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,13 +5,35 @@ const Intro: FC = () => {
         <div className="px-2">
             <div className="flex -mx-2">
                 <div className="w-1/3 px-2">
-                    <div className="bg-corn-500 rounded h-12"></div>
+                    <div className="bg-corn-500 flex rounded p-1 h-12">
+                        <div className="w-1/10 bg-black text-white bg-opacity-75 rounded-full h-10 w-10 flex items-center justify-center">
+                            1
+                        </div>
+                        <div className="w-9/10 p-1 text-white">
+                            Use the search bar to find a book
+                        </div>
+                    </div>
                 </div>
                 <div className="w-1/3 px-2">
-                    <div className="bg-corn-500 rounded h-12"></div>
+                    <div className="flex bg-corn-500 rounded p-1 h-12">
+                        <div className="w-1/10 bg-black text-white bg-opacity-75 rounded-full h-10 w-10 flex items-center justify-center">
+                            2
+                        </div>
+                        <div className="w-9/10 p-1 text-white">
+                            Click on calendar to schedule book
+                        </div>
+                    </div>
                 </div>
                 <div className="w-1/3 px-2">
-                    <div className="bg-corn-500 rounded h-12"></div>
+                    <div className="bg-corn-500 flex rounded p-1 h-12">
+                        <div className="w-1/10 bg-black text-white bg-opacity-75 rounded-full h-10 w-10 flex items-center justify-center">
+                            3
+                        </div>
+                        <div className="w-9/10 p-1 text-white">
+                            View book and schedule information, and then add
+                            book
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
