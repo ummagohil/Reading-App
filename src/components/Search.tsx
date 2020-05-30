a34ab2724b2776e199876e5f84e89586590d55f3
u-gohil <756D6D61@gmail.com>
2020-05-30 10:12:51 +0100

    Removed space

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,7 +3,6 @@ import React, { FC } from 'react'
 const Search: FC = () => {
     return (
         <div className="px-2 p-4">
-            {' '}
             <input
                 className="text-corn-500 bg-black w-full h-16 rounded  p-2 focus:outline-none focus:outline-none text-xl"
                 type="search"
