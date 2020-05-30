2e17652b92e4cd0e819cb9c090590b20c064e159
u-gohil <756D6D61@gmail.com>
2020-05-30 10:14:15 +0100

    Minor change (opacity)

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -3,12 +3,12 @@ import React, { FC } from 'react'
 const Info: FC = () => {
     return (
         <div className="px-2 p-4">
-        <div className=" text-corn-500 rounded bg-black flex">
-        <div className="w-1/2 p-4">Book info</div>
-        <div className="border-r border-corn-500 p-4"></div>
-        <div className="w-1/2 p-4">Stats info</div>
-    </div>
-    </div>
+            <div className=" text-corn-500 rounded bg-black flex">
+                <div className="w-1/2 p-4">Book info</div>
+                <div className="border-r border-corn-500 p-4 opacity-50"></div>
+                <div className="w-1/2 p-4">Stats info</div>
+            </div>
+        </div>
     )
 }
 export default Info
